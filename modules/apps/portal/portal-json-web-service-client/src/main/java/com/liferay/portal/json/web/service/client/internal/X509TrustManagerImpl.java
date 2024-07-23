a1199cfe0e39603b2964ae5c50ba803cba99f237
Eva Budai <eva.budai@liferay.com>
2024-07-23 14:44:19 +0200

    LPP-54784 Check only CA certificates

--- modules/apps/portal/portal-json-web-service-client/src/main/java/com/liferay/portal/json/web/service/client/internal/X509TrustManagerImpl.java
+++ modules/apps/portal/portal-json-web-service-client/src/main/java/com/liferay/portal/json/web/service/client/internal/X509TrustManagerImpl.java
@@ -11,6 +11,11 @@ import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
@@ -79,10 +84,12 @@ public class X509TrustManagerImpl implements X509TrustManager {
 			X509Certificate[] x509Certificates, String authType)
 		throws CertificateException {
 
-		if (!_trustSelfSignedCertificates || (x509Certificates.length != 1)) {
+		X509Certificate[] caCertificates = _getCACertificates(x509Certificates);
+
+		if (!_trustSelfSignedCertificates || (caCertificates.length >= 1)) {
 			try {
 				_defaultX509TrustManager.checkServerTrusted(
-					x509Certificates, authType);
+					caCertificates, authType);
 			}
 			catch (CertificateException certificateException) {
 				if (_extraX509TrustManager != null) {
@@ -107,6 +114,33 @@ public class X509TrustManagerImpl implements X509TrustManager {
 		return _defaultX509TrustManager.getAcceptedIssuers();
 	}
 
+	private X509Certificate[] _getCACertificates(
+			X509Certificate[] x509Certificates)
+		throws CertificateException {
+
+		List<X509Certificate> caCertificates = Arrays.asList();
+
+		for (X509Certificate x509Certificate : x509Certificates) {
+			if (!_isSelfSignedCertificate(x509Certificate)) {
+				caCertificates.add(x509Certificate);
+			}
+		}
+
+		return caCertificates.toArray(new X509Certificate[0]);
+	}
+
+	private String _getOrganizationFromIssuerDN(String issuerDN) {
+		Pattern pattern = Pattern.compile("O=(.*?)(,|$)");
+
+		Matcher matcher = pattern.matcher(issuerDN);
+
+		if (matcher.find()) {
+			return matcher.group(1);
+		}
+
+		return null;
+	}
+
 	private X509TrustManager _getX509TrustManager(KeyStore keyStore)
 		throws Exception {
 
@@ -125,6 +159,29 @@ public class X509TrustManagerImpl implements X509TrustManager {
 		}
 
 		return null;
+	}
+
+	private boolean _isSelfSignedCertificate(X509Certificate cert)
+		throws CertificateException {
+
+		if (cert == null) {
+			throw new CertificateException(
+				"Certificate must be an X509Certificate");
+		}
+
+		String issuerDN = _getOrganizationFromIssuerDN(
+			cert.getIssuerDN(
+			).getName());
+		String subjectDN = _getOrganizationFromIssuerDN(
+			cert.getSubjectDN(
+			).getName());
+
+		if ((issuerDN == null) || (subjectDN == null)) {
+			throw new CertificateException(
+				"The issuer or the subject of certificate is missing");
+		}
+
+		return issuerDN.equals(subjectDN);
 	}
 
 	private final X509TrustManager _defaultX509TrustManager;
